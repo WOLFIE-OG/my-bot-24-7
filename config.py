e738b00371b62cb33e738700a4e75b4d70ed573a
WOLFIE-OG <35380754+WOLFIE-OG@users.noreply.github.com>
2018-09-11 16:44:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-token = '' NDg5MDkzNzYwODE1NDY0NDY5.DnlwBg.HA7mNofVnWypNNx13ObdX2iewfI
+token = 'NDg5MDkzNzYwODE1NDY0NDY5.DnlwBg.HA7mNofVnWypNNx13ObdX2iewfI' 
 
-prefix = '' >
+prefix = '>' 
 
-link = '' https://discordapp.com/api/oauth2/authorize?client_id=489093760815464469&permissions=8&scope=bot
+link = 'https://discordapp.com/api/oauth2/authorize?client_id=489093760815464469&permissions=8&scope=bot'
 
-ownerid = '' 489093760815464469
+ownerid = '489093760815464469' 
 
